35a44841fe10f78801ccb39d6e7726ebb3b6128e
asyzdykbekov <asyzdykbekov@phoenixit.ru>
2025-04-05 23:50:16 +0300

    add HW_05 ThirdSuiteTest.java, chg SecondSuiteTest.java

--- src/test/java/ThirdSuiteTest.java
+++ src/test/java/ThirdSuiteTest.java
@@ -1,14 +1,13 @@
-import net.bytebuddy.utility.dispatcher.JavaDispatcher;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
-@DisplayName(value = "Авторизация и удаление с явным и неявным ожиданием")
-public class SecondSuiteTest {
+@DisplayName(value = "Проверка заголовка и текста заметки")
+public class ThirdSuiteTest {
+
     WebDriver driver;
 
     @BeforeAll
@@ -21,28 +20,29 @@ public class SecondSuiteTest {
     }
 
     @Test
-    @DisplayName(value = "Тест авторизации с неявным ожиданием")
-    public void authorizationImplicitTest() {
+    @DisplayName(value = "Тест заголовка заметки")
+    public void noteTitleTest() {
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("http://172.24.120.5:8081/login");
         driver.findElement(By.xpath("//*[@id=\"login-input\"]")).sendKeys("login1");
         driver.findElement(By.xpath("//*[@id=\"password-input\"]")).sendKeys("passTest");
         driver.findElement(By.id("form_auth_button")).click();
-        driver.findElement(By.xpath("//*[@id=\"root\"]/div[1]/div[1]/a[2]")).click();
+        String titleText = driver.findElement(By.xpath("//*[@id=\"note-title-37\"]")).getText();
+        Assertions.assertEquals("Тестовый заголовок",titleText, "Неверный заголовок заметки");
     }
 
     @Test
-    @DisplayName(value = "Тест авторизации с явным ожиданием")
-    public void authorizationExplicitTest() {
+    @DisplayName(value = "Тест текста заметки")
+    public void noteTextTest() {
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("http://172.24.120.5:8081/login");
         driver.findElement(By.xpath("//*[@id=\"login-input\"]")).sendKeys("login1");
         driver.findElement(By.xpath("//*[@id=\"password-input\"]")).sendKeys("passTest");
         driver.findElement(By.id("form_auth_button")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"root\"]/div[1]/div[1]/a[2]"))).click();
+        String noteText = driver.findElement(By.xpath("//*[@id=\"note-content-37\"]")).getText();
+        Assertions.assertEquals("Текст заметки для проверки",noteText, "Неверный текст заметки");
     }
 
     @AfterEach
