a8c26849fea424b9e90aa379645f23c2687ed14d
asyzdykbekov <asyzdykbekov@phoenixit.ru>
2025-04-19 21:52:22 +0300

    add class LessonEight

---
+++ src/main/code/LessonEight.java
@@ -0,0 +1,34 @@
+package code;
+
+public class LessonEight {
+
+    public static void main(String[] args) {
+        // ДЗ 1
+
+        int number = 7778;
+        double decimal = 876.351;
+        String name = "Игорь";
+        char symbol = 'n';
+        boolean isTrue = true;
+
+        printer("Значение целочисленной переменной - " + number);
+        printer("Значение вещественной переменной - " + decimal);
+        printer("Значение строковой переменной - " + name);
+        printer("Значение символьной переменной - " + symbol);
+        printer("Значение логической переменной - " + isTrue + "\n");
+
+        // ДЗ 2
+        // Преобразование типов
+        double fromIntToDouble = (double) number;
+        int fromDoubleToInt = (int) decimal;
+
+        printer("Преобразование int " + number + " в double: " + fromIntToDouble);
+        printer("Преобразование double " + decimal + " в int: " + fromDoubleToInt);
+    }
+
+    private static void printer(String text) {
+        System.out.println(text);
+    }
+
+}
+
